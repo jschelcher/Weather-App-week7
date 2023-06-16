c671be3922505f2f2fe968b432c4c253ff8d2d44
Jordan Schelcher <jschelcher2022@gmail.com>
2023-06-16 09:12:47 -0400

    Changes suggested by Paul
    Still having issues with the columns not appearing

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
     console.log(coordinates);   
     let key = "d1a86552de255334f6117b348c4519bd";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&long=${coordinates.lon}&exclude={part}&appid=${apiKey}&unites=imperial`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&unites=imperial`;
 axios.get(apiUrl).then(displayForecast);
 }
 
@@ -90,7 +90,7 @@ function searchCity(city){
     let key = "d1a86552de255334f6117b348c4519bd";
     let url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`;
     
-    axios.get(url).then(currentWeather);rb
+    axios.get(url).then(currentWeather);
 
     }
 
@@ -105,4 +105,3 @@ function search(event) {
     let form = document.querySelector("#search-form");
     
     form.addEventListener("submit", search)
-displayForecast();
